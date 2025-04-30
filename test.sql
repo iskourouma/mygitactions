9b341b72d67d88ee3ec00c00c54631412c31fc53
iskourouma <ibrahima.s.kourouma@accenture.com>
2025-04-30 14:11:58 -0400

    Update test.sql

--- test.sql
+++ test.sql
@@ -1,1 +1,8 @@
-select * from table1;
+SELECT
+   col_a
+   , col_b
+   -- Newline present before column
+   , col_c
+   -- When inline, comma should still touch element before.
+   , GREATEST(col_d, col_e) as col_f
+FROM tbl_a
