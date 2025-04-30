227f6b779164d1b4e46d937d0bd4980ad7741cf6
iskourouma <ibrahima.s.kourouma@accenture.com>
2025-04-30 16:48:43 -0400

    Update test.sql

--- test.sql
+++ test.sql
@@ -1,33 +1,33 @@
- -- GROUP BY: Explicit
- SELECT
-     foo,
-     bar,
-     sum(baz) AS sum_value
- FROM fake_table
- GROUP BY
-     foo, bar;
+-- GROUP BY: Explicit
+SELECT
+    foo,
+    bar,
+    sum(baz) AS sum_value
+FROM fake_table
+GROUP BY
+    foo, bar;
 
- -- ORDER BY: Explicit
- SELECT
-     foo,
-     bar
- FROM fake_table
- ORDER BY
-     foo, bar;
+-- ORDER BY: Explicit
+SELECT
+    foo,
+    bar
+FROM fake_table
+ORDER BY
+    foo, bar;
 
- -- GROUP BY: Implicit
- SELECT
-     foo,
-     bar,
-     sum(baz) AS sum_value
- FROM fake_table
- GROUP BY
-     1, 2;
+-- GROUP BY: Implicit
+SELECT
+    foo,
+    bar,
+    sum(baz) AS sum_value
+FROM fake_table
+GROUP BY
+    1, 2;
 
- -- ORDER BY: Implicit
- SELECT
-     foo,
-     bar
- FROM fake_table
- ORDER BY
-     1, 2;
+-- ORDER BY: Implicit
+SELECT
+    foo,
+    bar
+FROM fake_table
+ORDER BY
+    1, 2;
