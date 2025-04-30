c3c3c090bcbea3e912595c8f8403ce620faf4bc8
iskourouma <ibrahima.s.kourouma@accenture.com>
2025-04-30 15:02:08 -0400

    Update test.sql

--- test.sql
+++ test.sql
@@ -1,8 +1,8 @@
 SELECT
-   	col_a,
-   	col_b,
-   -- Newline present before column
-   	col_c,
-   -- When inline, comma should still touch element before.
-   	GREATEST(col_d, col_e) as col_f
+    col_a,
+    col_b,
+-- Newline present before column
+    col_c,
+-- When inline, comma should still touch element before.
+    GREATEST(col_d, col_e) as col_f
 FROM tbl_a
