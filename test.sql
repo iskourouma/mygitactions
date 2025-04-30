82b7890c2673ea37839192e0d865c6699ce28b23
iskourouma <ibrahima.s.kourouma@accenture.com>
2025-04-30 16:53:56 -0400

    Update test.sql

--- test.sql
+++ test.sql
@@ -14,20 +14,19 @@ SELECT
 FROM fake_table
 ORDER BY
     foo, bar;
-
 -- GROUP BY: Implicit
-SELECT
-    foo,
-    bar,
-    sum(baz) AS sum_value
-FROM fake_table
-GROUP BY
-    1, 2;
+--SELECT
+--    foo,
+--    bar,
+--    sum(baz) AS sum_value
+--FROM fake_table
+--GROUP BY
+--    1, 2;
 
 -- ORDER BY: Implicit
-SELECT
-    foo,
-    bar
-FROM fake_table
-ORDER BY
-    1, 2;
+--SELECT
+--    foo,
+--    bar
+--FROM fake_table
+--ORDER BY
+--    1, 2;
